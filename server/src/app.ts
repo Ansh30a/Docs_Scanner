81070d34ada540aa9c9f1938b762e97d76d3b5a5
Ansh <anshuman302004@gmail.com>
2026-01-22 02:04:41 +0530

    update(cors): allow cors for firebase

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import uploadRoutes from './routes/uploadRoutes.js';
-import path from 'node:path';
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://anshuman-doc-scanner.web.app',
+    'https://anshuman-doc-scanner.firebaseapp.com'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
 app.use(express.json());
 
-app.use(
-    '/uploads',
-    express.static(path.join(process.cwd(), "uploads"))
-);
-
-app.use(
-    '/api/upload',
-    uploadRoutes
-);
+app.use('/api/upload', uploadRoutes);
 
 export default app;
